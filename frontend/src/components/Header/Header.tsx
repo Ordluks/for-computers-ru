6b291052f958bd71ac70d8d11ebbbc93d43efc8d
Ordluks <68952193+Ordluks@users.noreply.github.com>
2021-12-16 22:53:46 +0400

    Added navigation links to header, Hover effect for categories links

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -8,6 +8,13 @@ const Header: React.FC = () => {
 	return (
 		<div className={scss.wrapper}>
 			<h1 className={scss.siteName}><NavLink to='/'>ForComputers.ru</NavLink></h1>
+			<nav>
+				<ul>
+					<li><NavLink to='/'>Главная</NavLink></li>
+					<li><NavLink to='/products'>Товары</NavLink></li>
+					<li><NavLink to='/about'>О нас</NavLink></li>
+				</ul>
+			</nav>
 			<AccountInfo />
 		</div>
 	)
